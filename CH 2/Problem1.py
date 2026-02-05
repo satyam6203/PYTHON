93c1d298b471b331452bae0e13eecc2582de2025
satyam6203 <satyamkumarsinghjaisidih@gmail.com>
2026-02-06 00:24:05 +0530

    operator completed

---
+++ CH 2/Problem1.py
@@ -0,0 +1,21 @@
+a=int(input("Enter the number : "))
+b=int(input("Enter the number : "))
+
+sum=a+b
+print("sum = ",sum)
+
+rem=a%b
+print("remender is : ",rem)
+
+print(type(a)) # this print #class <int> because it is typecasted
+
+print("a is greater then b : ",a>b) 
+
+avg=sum/2
+print("the average of the a and b is : ",avg)
+
+# we can use the '**' to print the square of the number 
+print("square of ",a," is : ",a**2)
+print("square of ",a," is : ",a*a)
+# print("square of ",a," is : ",a^a) this not valied in the python
+
